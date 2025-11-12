c4a3e9e09034529e50d049fb341daec2e528ff03
Yanitsa Toncheva <yanitsa.toncheva87@gmail.com>
2025-11-12 14:55:06 +0200

    Add express to api

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,15 @@
-console.log('it works')
+import express from 'express';
+
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    console.log('it works')
+    res.send('it works!!!');
+});
+
+app.listen(3030, () => console.log('Server is listening on http://localhost:3030...'));
+
+
